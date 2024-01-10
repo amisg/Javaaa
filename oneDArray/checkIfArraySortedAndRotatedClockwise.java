87bf5c2f0482e40185c61a8129ec4a6d07e6ebb7
amisg <aamithsg18@gmail.com>
2024-01-10 22:17:39 +0530

    class name capitalized

--- oneDArray/checkIfArraySortedAndRotatedClockwise.java
+++ oneDArray/checkIfArraySortedAndRotatedClockwise.java
@@ -3,7 +3,7 @@ package oneDArray;
 import java.util.Arrays;
 import java.util.Scanner;
 
-public class checkIfArraySortedAndRotatedClockwise {
+public class CheckIfArraySortedAndRotatedClockwise {
 
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
