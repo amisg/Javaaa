80e488850eafec0dc8d6946977663dd8e0aedf11
amisg <aamithsg18@gmail.com>
2024-01-11 16:03:44 +0530

    Mock question

---
+++ oneDArray/DigitMaxSum.java
@@ -0,0 +1,39 @@
+package oneDArray;
+
+import java.util.Scanner;
+
+public class DigitMaxSum {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt();
+        int[] arr = new int[n];
+        for (int i = 0; i < n; i++) {
+            arr[i] = sc.nextInt();
+        }
+        int result = sumOfMax(arr);
+        System.out.println(result);
+        sc.close();
+    }
+    static int sumOfMax(int arr[]){
+        int sum = 0;
+        for (int num : arr) {
+            int digit = digitMax(num);
+            sum += digit;  
+        }
+        return sum;
+    }
+    static int digitMax(int num){
+        int maxDigit = 0;
+        while (num > 0) {
+            int digit = num % 10;
+            maxDigit = Math.max(maxDigit,digit);
+            num /= 10;
+        }
+        return maxDigit;
+    }
+}
+// intput
+// 4
+// 234 456 567 678
+// output
+// 25
