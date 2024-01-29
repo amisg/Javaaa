4af967175b1ced960916a590696164ec9fb7ea5b
amisg <aamithsg18@gmail.com>
2024-01-29 12:44:14 +0530

    fibinocci not using recurrsion

---
+++ basicOFPrograming/FibinocciPrint.java
@@ -0,0 +1,19 @@
+package basicOFPrograming;
+
+import java.util.Scanner;
+
+public class FibinocciPrint {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt();
+        int a = 0;
+        int b = 1;
+        for (int i = 1; i <= n; i++) {
+            System.out.println(a);
+            int c = a + b;
+            a = b;
+            b = c;
+        }
+        sc.close();
+    }
+}
