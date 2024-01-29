0316849cc45028a031545a361d0927fa48875c74
amisg <aamithsg18@gmail.com>
2024-01-29 23:05:05 +0530

    patterns

---
+++ patterns/InvertedRightAllingedTriangle.java
@@ -0,0 +1,24 @@
+package patterns;
+
+import java.util.Scanner;
+
+public class InvertedRightAllingedTriangle {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt();
+        int space = 0;
+        int star = n;
+        for(int i=1;i<=n;i++){      
+            for(int j=1;j<=space;j++){
+                System.out.print("  ");
+            }
+            for(int j=1;j<=star;j++){
+                System.out.print("*");
+            }
+            space++;
+            star--;
+            System.out.println();
+        }
+        sc.close();
+    }
+}
