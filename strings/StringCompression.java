9417f95d2d028eee2ac2a4f5b05fe25aea39f259
amisg <aamithsg18@gmail.com>
2024-02-11 19:33:01 +0530

    string compression

---
+++ strings/StringCompression.java
@@ -0,0 +1,56 @@
+package strings;
+
+import java.util.Scanner;
+
+public class StringCompression {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        String s = sc.next();
+        String ans1 = compression1(s);
+        System.out.println(ans1);
+        String ans2 = compression2(s);
+        System.out.println(ans2);
+        sc.close();
+    }
+    public static String compression1(String str){
+        String s = str.charAt(0) + "";
+        for (int i = 1; i < str.length(); i++) {
+            char curr = str.charAt(i);
+            char prev = str.charAt(i - 1);
+            if (curr != prev) {
+                s += curr;
+            }
+        }
+        return s;
+    }
+    public static String compression2(String str){
+        String s = str.charAt(0) + "";
+        int count = 1;
+        for (int i = 1; i < str.length(); i++){
+            char curr = str.charAt(i);
+            char prev = str.charAt(i - 1);
+            if (curr == prev) {
+                count++;
+            } else {
+                if (count > 1) {
+                    s += count;
+                    count = 1;
+                }
+                s += curr;
+            }
+        }
+        if (count > 1) {
+            s += count;
+            count = 1;
+        }
+        return s;
+    }
+}
+
+
+
+//input
+// aaabbbccdeeef
+//output
+// abcdef
+// a3b3c2de3f
