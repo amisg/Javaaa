96f5cfb989cb76aa95c217d52ca185f2eda833ae
amisg <aamithsg18@gmail.com>
2024-09-24 15:42:39 +0530

    convert toCharArray and remove

---
+++ strings/VowelRemoval.java
@@ -0,0 +1,18 @@
+package strings;
+
+import java.util.Scanner;
+
+public class VowelRemoval {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        String word = sc.next();
+        sc.close();
+        for (char ch : word.toCharArray()) {
+            if (ch=='a' || ch=='e' || ch=='i'|| ch=='o' || ch=='u') {
+                continue;
+            } else {
+                System.out.print(ch);
+            }
+        }
+    }
+}
