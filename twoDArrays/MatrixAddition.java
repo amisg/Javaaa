c5677b68a094d8fbb3ca35c2b88c2f1ac1ea2ce1
amisg <aamithsg18@gmail.com>
2024-09-27 20:01:20 +0530

    2d addition

---
+++ twoDArrays/MatrixAddition.java
@@ -0,0 +1,43 @@
+package twoDArrays;
+
+import java.util.Scanner;
+
+public class MatrixAddition {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int rows = sc.nextInt();
+        int cols = sc.nextInt();
+        int[][] arr1 = new int[rows][cols];
+        int[][] arr2 = new int[rows][cols];
+
+        for (int i = 0; i < rows; i++) {
+            for (int j = 0; j < cols; j++) {
+                arr1[i][j] = sc.nextInt();
+            }
+        }
+
+        
+        for (int i = 0; i < rows; i++) {
+            for (int j = 0; j < cols; j++) {
+                arr2[i][j] = sc.nextInt();
+            }
+        }
+
+        sc.close();
+
+        int[][] ans = new int[rows][cols];
+
+        for (int i = 0; i < rows; i++) {
+            for (int j = 0; j < cols; j++) {
+                ans[i][j] = arr1[i][j] + arr2[i][j];
+            }
+        }
+
+        for (int i = 0; i < rows; i++) {
+            for (int j = 0; j < cols; j++) {
+                System.out.print(ans[i][j]+" ");
+            }
+            System.out.println();
+        }
+    }
+}
