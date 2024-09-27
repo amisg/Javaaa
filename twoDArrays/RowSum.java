dd7d69c749ee92627c1239eeac5613312f3b920e
amisg <aamithsg18@gmail.com>
2024-09-27 20:30:33 +0530

    row sum

---
+++ twoDArrays/RowSum.java
@@ -0,0 +1,28 @@
+package twoDArrays;
+
+import java.util.Scanner;;
+
+public class RowSum {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int rows = sc.nextInt();
+        int cols = sc.nextInt();
+        int[][] arr = new int[rows][cols];
+
+        for (int i = 0; i < rows; i++) {
+            for (int j = 0; j < cols; j++) {
+                arr[i][j] = sc.nextInt();
+            }
+        }
+
+        sc.close();
+
+        for (int i = 0; i < rows; i++) {
+            int rowSum = 0;
+            for (int j = 0; j < cols; j++) {
+                rowSum += arr[i][j];
+            }
+            System.out.println(rowSum);
+        }
+    }
+}
